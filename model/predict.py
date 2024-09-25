77f9b050718c0e7b981911086e2a2f28f1d665d6
Pranav Papnai <ug_40822021@dseu.ac.in>
2024-09-25 20:40:58 +0530

    first commit

---
+++ model/predict.py
@@ -0,0 +1,7 @@
+import joblib
+import numpy as np
+def predict(input_features):
+ # Load the trained model
+   model = joblib.load('model.pkl')
+   prediction = model.predict(np.array([input_features]))
+   return prediction
